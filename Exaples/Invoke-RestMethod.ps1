dacdba754bdc07f77e42c65c8adc95652e505b97
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2023-01-12 16:39:37 +0200

    Update Invoke-RestMethod.ps1

--- Exaples/Invoke-RestMethod.ps1
+++ Exaples/Invoke-RestMethod.ps1
@@ -5,11 +5,10 @@ $Data = @{
 } | ConvertTo-Json
 
 $UserPass = "yoavklein:Yoav-Klein3"
-$Bytes = [System.Text.Encoding]::Unicode.GetBytes($UserPass)
-$EncodedText =[Convert]::ToBase64String($Bytes)
+#$UserPass = "cloud_user:WRRW6448hmjo"
+$Bytes = [System.Text.Encoding]::ASCII.GetBytes($UserPass)
+$EncodedText = [System.Convert]::ToBase64String($Bytes)
 $Headers = @{
     Authorization = "Basic $EncodedText"
 }
 $Url = "https://search-movies-lahamhucuj7pfevsvuek4jwruu.us-east-1.es.amazonaws.com/movies/_doc/1"
-#$Url = "http://localhost:9200/some_ind/_doc/1"
-Invoke-RestMethod -Headers $Headers -ContentType "application/json" -Body $Data -Uri $Url -Method PUT
