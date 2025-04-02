1172e043a2c191b619f62e5a91d0c2dd21d76d09
yoklein <yoav.klein@cyberark.com>
2025-04-02 18:10:51 +0300

    Select-STring

--- Examples/string-manipulation/01-using-select-string/05-find-surrounding/Find-Surrounding.ps1
+++ Examples/string-manipulation/01-using-select-string/05-find-surrounding/Find-Surrounding.ps1
@@ -2,7 +2,18 @@
 # get lines before and after the match
 # using the -context parameter
 
-Select-String -Path example.txt -Pattern "example" -Context 2,2
+$result = Select-String -Path example.txt -Pattern "example" -Context 2,2
+
+# The context is stored in a Context field of the MatchInfo objects
+# It's a MatchInfoContext object
+
+$result | % {
+    #$_.Context.GetType()
+    echo "=== before"
+    $_.Context.PreContext
+    echo "=== after"
+    $_.Context.PostContext
+}
 
 # you can see that the output marks > on the lines matched
 # and outputs 2 lines before and 2 lines after each match
